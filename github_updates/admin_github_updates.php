0e6ec3794c49a39ae4a91adae25cc68a2c64e035
Rayth <ramorrison1992@gmail.com>
2017-03-12 03:43:57 +0000

    Github Commit Listing
    Pulls commits from github and stores in database.
    
    Installation:
    - Upload to your modules folder under Hypersite Root directory
    - Goto Admin CP > Module Management > Unloaded Modules > github_updates
    > Load
    - Goto Admin CP > Site Settings
    - Fill in Github User and Repository, save
    - Goto Admin CP > github_updates
    - Click update button.

---
+++ github_updates/admin_github_updates.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * @package         HyperSite CMS
+ * @file            admin_test_mod.php
+ * @file_desc       Test Modul
+ * @author          Ryan Morrison
+ * @website         http://www.hypersite.info
+ * @copyright       (c) 2017 HyperSite CMS
+ * @license         http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+/* Check if we are in CMS otherwise exit code. */
+if (!defined('IN_HSCMS'))
+{
+	exit;
+}
+/* Check if in ACP */
+if (!defined('IN_ACP'))
+{
+    exit;
+}
+/* Main code Here */
+$do = request_var('do', 'none');
+define('GITHUB_TABLE', $config->mysql['table_prefix'] . 'github');
+$template_file .= "modules/github_updates/styles/{$config->config['site_theme']}/template/admin_github.html";
+$template->assign_var('PAGE_TITLE', 'Github ACP');
+switch ($do)
+{
+    case 'update':
+        $url = 'https://api.github.com/repos/'. $config->config['github_user'] . '/' . $config->config['github_repo'] . '/commits';
+        $curl = curl_init();
+        curl_setopt($curl,CURLOPT_USERAGENT,'HyperSite CMS - Github Module 1.0 (http://www.hypersite.info)');
+        $timeout = 30;
+        curl_setopt($curl, CURLOPT_URL, utf8_encode($url));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $result = curl_exec($curl);
+        $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if($responseCode == 200 && $result)
+        {
+            $commits = json_decode($result, true);
+            foreach ($commits as $commit)
+            {
+                $info = array(
+                    'commit_author' => $db->clean($commit['commit']['author']['name']),
+                    'commit_time' => strtotime($commit['commit']['author']['date']),
+                    'commit_sha' => $commit['sha']
+                );
+                $query = "SELECT * FROM " . GITHUB_TABLE . " WHERE commit_sha='{$commit['sha']}'";
+                $sharesult = $db->query($query);
+                $sharow = $db->fetchrow($sharesult);
+                if (!$sharow) 
+                {
+                    $query = $db->build_query('insert', GITHUB_TABLE, $info);
+                    $result = $db->query($query);
+                    if (!$result)
+                    {
+                        $template->assign_vars(array(
+                            'ERROR' => 1,
+                            'ERROR_MSG' => 'Failed to put into Database:<br/>' . $query
+                        ));
+                        break;
+                    }
+                }
+            }
+            $template->assign_vars(array(
+               'MESSAGE' => 1,
+                'TEXT' => 'Successfully updated the database. Added ' . count($commits) . ' Commits.'
+            ));
+        }
+        else
+        {
+            $template->assign_vars(array(
+                'ERROR' => 1,
+                'ERROR_MSG' => 'Could not retrieve git list fomr github. Please check settings.'
+            ));
+        }
+        break;
+    default:
+    case 'none':
+        $template->assign_vars(array(
+            'ERROR' => 0,
+            'MESSAGE' => ''
+        ));
+        break;
+}
+
+
---
+++ github_updates/github_updates.php
@@ -0,0 +1,109 @@
+<?php
+/*
+ * @package         HyperSite CMS
+ * @file            test_mod.php
+ * @file_desc       A Test module
+ * @author          Ryan Morrison
+ * @website         http://www.hypersite.info
+ * @copyright       (c) 2017 HyperSite CMS
+ * @license         http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+ /* Check if we are in CMS otherwise exit code. */
+if (!defined('IN_HSCMS'))
+{
+	exit;
+}
+
+/* Main Code here */
+define('GITHUB_TABLE', $config->mysql['table_prefix'] . 'github');
+$template_file .= "modules/github_updates/styles/{$config->config['site_theme']}/template/";
+$template->assign_var('PAGE_TITLE', 'CMS Updates');
+switch ($act)
+{
+    case 'view':
+        $sha = $i;
+        if ($sha != null)
+        {
+            $url = "https://api.github.com/repos/{$config->config['github_user']}/{$config->config['github_repo']}/commits/{$sha}";
+            $curl = curl_init();
+            curl_setopt($curl,CURLOPT_USERAGENT,'HyperSite CMS - Github Module 1.0 (http://www.hypersite.info)');
+            $timeout = 30;
+            curl_setopt($curl, CURLOPT_URL, utf8_encode($url));
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout);
+            $result = curl_exec($curl);
+            $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            if($responseCode == 200 && $result){
+                $commit_info = json_decode($result, true); 
+                $template->assign_vars(array(
+                    'AUTHOR' => $commit_info['commit']['author']['name'],
+                    'DATE' => $commit_info['commit']['author']['date'],
+                    'MESSAGE' => nl2br($commit_info['commit']['message'])
+                )); 
+                foreach ($commit_info['files'] as $com_file)
+                {
+                    if ($com_file['status'] == 'added')
+                    {
+                        $template->assign_block_vars('files_added', array(
+                            'NAME' => $com_file['filename'],
+                        ));
+                    }
+                    else if ($com_file['status'] == 'modified')
+                    {
+                        $template->assign_block_vars('files_changed', array(
+                            'NAME' => $com_file['filename'],
+                        ));
+                    }
+                    else
+                    {
+                        $template->assign_block_vars('files_deleted', array(
+                            'NAME' => $com_file['filename'],
+                        ));
+                    }
+                }
+                
+            }else{
+                $template->assign_vars(array(
+                    'ERROR' => 1,
+                    'ERROR_MSG' => 'Failed to get Information for Commit.'
+                ));
+            }
+        }
+        else
+        {
+            $template->assign_vars(array(
+                'ERROR' => 1,
+                'ERROR_MSG' => 'No commit specified.'
+            ));
+        }
+        $template_file .= 'view_commit.html';
+        break;
+    default:
+        $query = "SELECT * FROM " . GITHUB_TABLE . " ORDER BY commit_time DESC LIMIT 100";
+        $result = $db->query($query);
+        if ($result)
+        {
+            $commits = $db->fetchall($result);
+            foreach ($commits as $commit)
+            {
+                $commit['commit_time'] = date('c', $commit['commit_time']);
+                foreach ($commit as $key => $val)
+                {
+                    $ukey[strtoupper($key)] = $val;
+                }
+                $commit = $ukey;
+                $template->assign_block_vars('commits', $commit);
+            }
+        }
+        else 
+        {
+            $template->assign_vars(array(
+                'ERROR' => 1,
+                'ERROR_MSG' => 'Unable to gather commits from database.'
+            ));
+        }
+        $template_file .= "commits.html";
+        break;
+}
+    
